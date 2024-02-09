78d65a4649b8015907c589c580fd24cc711c34dc
Dmytro <savchyndd@gmail.com>
2024-02-09 08:10:13 +0100

    sample: added header, footer, home and region pages for sample

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './HomePage.module.scss';
 
-export default function Home() {
+const HomePage = () => {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+        <p>Home page </p>
       </div>
     </main>
   );
-}
+};
+
+export default HomePage;
---
+++ src/components/Footer/footer.tsx
@@ -0,0 +1,11 @@
+import './footer.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
